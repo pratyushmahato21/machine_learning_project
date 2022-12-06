5e965aa9a5276219102ddbe3821a9efd8a35c753
Pratyush Mahato <pratyushmahato21@gmail.com>
2022-12-06 14:16:48 +0530

    updated

--- setup.py
+++ setup.py
@@ -1,35 +1,46 @@
-from setuptools import setup
-from typing import List
+from setuptools import setup, find_packages
+#from typing import List
 
-
-def get_requirements_list() -> List[str]:
-    """
-    Description: This function is going to return list of requirements mentioned in requirements.txt file
-
-    return This function is going to return a list which contain names of libraries mentioned in requirements.txt file 
-    """
-    with open(REQUIREMENTS_FILE_NAME) as requirements_file:
-        return requirements_file.readlines()
 
 # Declaring variables for setup functions
-PROJECT_NAME = "house_price_prediction"
-VERSION = "0.0.1"
-AUTHOR = "Pratyush Mahato"
-DESCRIPTION = "This ML project predicts the price of a house"
-PACKAGES = ["housing"]
-REQUIREMENTS_FILE_NAME = "requirements.txt"
+#PROJECT_NAME = "house_price_prediction"
+#VERSION = "0.0.3"
+#AUTHOR = "Pratyush Mahato"
+#DESCRIPTION = "This ML project predicts the price of a house"
 
+#REQUIREMENTS_FILE_NAME = "requirements.txt"
+
+
+#def get_requirements_list() -> List[str]:
+#   """
+#    Description: This function is going to return list of requirements mentioned in requirements.txt file
+#
+#    return This function is going to return a list which contain names of libraries mentioned in requirements.txt file 
+ #   """
+  #  with open(REQUIREMENTS_FILE_NAME) as requirements_file:
+   #     return requirements_file.readlines().pop("-e .")
+
+
+
+#setup(
+#name= PROJECT_NAME,
+#version= VERSION,
+#author= AUTHOR,
+#description = DESCRIPTION,
+#packages= find_packages(),
+#install_requires = get_requirements_list()
+
+#)
+
+from setuptools import find_packages, setup
 
 setup(
-name= PROJECT_NAME,
-version= VERSION,
-author= AUTHOR,
-description = DESCRIPTION,
-packages= PACKAGES,
-install_requires = get_requirements_list()
-
+    name="house_price_prediction",
+    version="0.0.5",
+    author="Pratyush Mahato",
+    description="This ML project predicts the price of a house",
+    packages=find_packages(),
+    install_requires=[],
 )
 
 
-
-    
